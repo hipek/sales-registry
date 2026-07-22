8f7155932d6dd97d6652da26ea80e2a1edc38d51
Jacek Hiszpanski <jhiszpanski@solvd.com>
2026-07-22 15:11:40 +0200

    chore: update deploy-compose.yaml

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /Users/jacek/work/3d-sales-registry
+git add deploy-compose.yaml
+git commit -m "feat(deploy): add frontend env vars for NEXT_PUBLIC_APP_URL, BACKEND_API_URL, NEXT_PUBLIC_API_URL"
